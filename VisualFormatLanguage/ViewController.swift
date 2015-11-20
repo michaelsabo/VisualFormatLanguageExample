4261711f9941c0a8f5206f1663a4a304fdaa9bff
Mike Sabo <mike.sabo614@gmail.com>
2015-11-19 23:47:28 -0500

    Initial Commit

---
+++ VisualFormatLanguage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VisualFormatLanguage
+//
+//  Created by Mike Sabo on 11/19/15.
+//  Copyright © 2015 Mike Sabo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
